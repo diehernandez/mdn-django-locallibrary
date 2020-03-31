244a7d8765c2854a2c8408cf3b8725229ccfbc2d
Diego <camilahernandezcatalan@MacBook-Pro-de-Camila.local>
2020-03-30 21:53:35 -0300

    add generic forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,12 +4,32 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
-    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
-    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    url(r'^allbooks/$', views.LoanedBooksListView.as_view(), name='all-borrowed'),
-    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>',views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path(r'borrowed/', views.LoanedBooksListView.as_view(), name='all-borrowed'),  # Added for challenge
+]
+
+# Add URLConf for librarian to renew a book.
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+# Add URLConf to create, update, and delete authors
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+]
+# Add URLConf to create, update, and delete books
+urlpatterns += [
+    path('book/create/', views.BookCreate.as_view(), name='book_create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,13 @@
 import datetime
 
-from django.shortcuts import get_object_or_404, render
-from django.views import generic
+from django.forms import ModelForm
 #from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import permission_required
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+
+
 
 
 from .models import Book, Author, BookInstance, Genre
-from .forms import RenewBookForm
 
-def ejemplos(request):
-    return render(request,'ejemplos.html')
 
 def index(request):
     """
@@ -38,7 +32,7 @@ def index(request):
     }
     return render(request,'catalog/index.html',context)
 
-
+from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
@@ -51,6 +45,8 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
     """
@@ -75,6 +71,13 @@ class LoanedBooksListView(PermissionRequiredMixin,generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact='o').order_by('due_back')
 
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import permission_required
+
+from .forms import RenewBookForm
+
 @permission_required('catalog.can_mark_returned')
 def renew_book_librarian(request, pk):
     book_inst =get_object_or_404(BookInstance, pk = pk)
@@ -97,3 +100,41 @@ def renew_book_librarian(request, pk):
         form = RenewBookForm(initial={'renewal_date': proposal_renewed_date,})
 
     return render( request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Author
+
+class AuthorCreate(PermissionRequiredMixin,CreateView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death':'05/01/2018'}
+
+class AuthorUpdate(PermissionRequiredMixin,UpdateView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    fields  = ['first_name','last_name','date_of_birth','date_of_death']
+
+class AuthorDelete(PermissionRequiredMixin,DeleteView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    success_url = reverse_lazy('authors')
+
+class BookCreate(PermissionRequiredMixin,CreateView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Book
+    fields = '__all__'
+    template_name = 'catalog/author_form.html'
+class BookUpdate(PermissionRequiredMixin,UpdateView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Book
+    fields = '__all__'
+    template_name = 'catalog/author_form.html'
+class BookDelete(PermissionRequiredMixin,DeleteView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Book
+    emplate_name = 'catalog/author_confirm_delete.html'
+    success_url = reverse_lazy('books')
